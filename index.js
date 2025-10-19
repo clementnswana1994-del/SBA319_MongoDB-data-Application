07103b47275ff66a91735068f5c79979ec8c444d
clementnswana1994-del <clementnswana1994@gmail.com>
2025-10-19 00:20:58 -0400

    first commit

--- index.js
+++ index.js
@@ -1,69 +1,88 @@
-import express from 'express'
+import express from "express";
 
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
-import Product from './Data/product.js'
+import Product from "./Data/product.js";
 
-const port = 3000
+const port = 3000;
 
-const connectionString = 'mongodb+srv://clementnswana1994_db_user:Chomba@cluster0.bt4oawr.mongodb.net/My_Data?retryWrites=true&w=majority&appName=Cluster0'
-await mongoose.connect(connectionString)
+const connectionString =
+  "mongodb+srv://clementnswana1994_db_user:Chomba@cluster0.bt4oawr.mongodb.net/My_Data?retryWrites=true&w=majority&appName=Cluster0";
+await mongoose.connect(connectionString);
 
-console.log('connect to MongoDB');
+console.log("connect to MongoDB");
 const app = express();
 
 app.use(express.json());
 
 // Define a simple route
-app.get('/', async (req, res) => {
-res.send('Server is running!');
+app.get("/", async (req, res) => {
+  res.send("Server is running!");
 });
 
-
-// Create a new user
-app.post('/api/products', async (req, res) => {
-try {
+// Create a new product
+app.post("/api/products", async (req, res) => {
+  try {
     const product = await Product.create(req.body);
-// const product = await productDoc.save(req.body);
+    // const product = await productDoc.save(req.body);
     res.status(201).json(product);
- } catch (error) {
+  } catch (error) {
     res.status(400).json({ message: error.message });
- }
+  }
 });
 
- // Get all users
- app.get('/api/products', async (req, res) => {
- try {
-    const products = await Product.find({})
-    res.status(200).json(products);
- } catch (error) {
- res.status(500).json({ message: error.message });
- }
+// Get all Products
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(201).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
-// // Update a user by ID
-// app.put('/users/:id', async (req, res) => {
-// try {
-// const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
-// if (!user) return res.status(404).json({ error: 'User not found' });
-// res.json(user);
-// } catch (error) {
-// res.status(400).json({ message: error.message });
-// }
-// });
+// Get one Product
+app.get("/api/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(201).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
-// // Delete a user by ID
-// app.delete('/users/:id', async (req, res) => {
-// try {
-// const user = await User.findByIdAndDelete(req.params.id);
-// if (!user) return res.status(404).json({ error: 'User not found' });
-// res.json({ message: 'User deleted successfully' });
-// } catch (error) {
-// res.status(500).json({ message: error.message });
-// }
-// });
+// Update a Product by ID
+app.put("/api/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    const updatedProduct = await Product.findById(id);
+    res.status(201).json(updatedProduct);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Delete a user by ID
+app.delete("/api/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    res.status(200).json({ message: "Product deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 // Start the server
 app.listen(3000, async () => {
-    console.log('Listening on port:', port)
-})
+  console.log("Listening on port:", port);
+});
